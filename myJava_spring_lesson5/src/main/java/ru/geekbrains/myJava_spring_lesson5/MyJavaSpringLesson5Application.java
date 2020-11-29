ad217c2a882b90fcb30021304af81c28b891a656
listpv <listpv@mail.ru>
2020-11-29 13:42:36 +0300

    Initial commit

---
+++ myJava_spring_lesson5/src/main/java/ru/geekbrains/myJava_spring_lesson5/MyJavaSpringLesson5Application.java
@@ -0,0 +1,13 @@
+package ru.geekbrains.myJava_spring_lesson5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyJavaSpringLesson5Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyJavaSpringLesson5Application.class, args);
+	}
+
+}
---
+++ myJava_spring_lesson5/src/test/java/ru/geekbrains/myJava_spring_lesson5/MyJavaSpringLesson5ApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.geekbrains.myJava_spring_lesson5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyJavaSpringLesson5ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
